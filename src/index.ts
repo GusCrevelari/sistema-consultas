1cb88e423a6cb6e6aa25fec0b20bc831cd88e46b
GusCrevelari <gustavocrevelari@hotmail.com>
2026-02-25 20:22:12 -0300

    type error fixes across index.ts, consulta.ts and medico.ts;

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-import { Especialidade } from "./types/especialidade";
-import { Paciente } from "./types/paciente";
-import { StatusConsulta } from "./types/statusConsulta";
-import { Medico } from "./interfaces/medico";
-import { Consulta } from "./interfaces/consulta";
+import type { Especialidade } from "./types/especialidade";
+import type { Paciente } from "./types/paciente";
+import type { StatusConsulta } from "./types/statusConsulta";
+import type { Medico } from "./interfaces/medico";
+import type { Consulta } from "./interfaces/consulta";
 
 const cardiologia: Especialidade = {
   id: 1,
